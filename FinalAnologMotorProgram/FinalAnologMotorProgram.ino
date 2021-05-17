e5fc6b51e7d1f9ded45cf59cd97cc55c134df961
Fernando-Matias <fm278841@gmail.com>
2021-05-17 10:54:20 -0700

    Update FinalAnologMotorProgram.ino

--- FinalAnologMotorProgram/FinalAnologMotorProgram.ino
+++ FinalAnologMotorProgram/FinalAnologMotorProgram.ino
@@ -1,12 +1,12 @@
 /*
- * Author: Fernando E. Matias
- * Anolog Boost/Vaccum Gauge
- * Commissioned by Digital Metrology Works LLC
- */
+   Author: Fernando E. Matias
+   Anolog Boost/Vaccum Gauge
+   Commissioned by Digital Metrology Works LLC
+*/
 
 // Include the AccelStepper Library
 #include <AccelStepper.h>
- 
+
 // Define Constants
 int targetPositionPSI;
 int stepperPosition;
@@ -22,7 +22,11 @@ bool homeSeq2 = false;
 int upStepps = -14;
 int downStepps = 7;
 
- // const varaible needed for calculations and comletetion of cuircuts
+int motorMax = 155;
+int motorSpeed = 150;
+int motorAccel = 120;
+
+// const varaible needed for calculations and comletetion of cuircuts
 const int preasureInput = A1; //PIN A1
 const double preasureZero = 102.4; //Anlog reading of transducer at 0 PSI
 const double preasureMax = 921.6; //Anlog reading of transducer at 30 PSI
@@ -31,13 +35,13 @@ const int baudRate = 9600; //Serial moniter
 const int sensorReadDelay = 250; //delay in millliseconds
 
 float preasureValue = 0; //varible for storing the preasure from the transducer
- 
+
 // Define step constants
 #define FULLSTEP 4
 #define HALFSTEP 8
- 
+
 // Define Motor Pins (2 Motors used)
- 
+
 #define motorPin1  8     // Blue   - 28BYJ48 pin 1
 #define motorPin2  9     // Pink   - 28BYJ48 pin 2
 #define motorPin3  10    // Yellow - 28BYJ48 pin 3
@@ -48,49 +52,49 @@ float preasureValue = 0; //varible for storing the preasure from the transducer
 AccelStepper stepper1(FULLSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
 
 void setup()
-{ 
-  Serial.begin(28800);
+{
+  Serial.begin(9600);
   // 1 revolution Motor 1 CW
-  stepper1.setMaxSpeed(1000.0);
-  stepper1.setAcceleration(100.0);
-  stepper1.setSpeed(400);
-  while(homeSeq1 == false){
-    stepper1.moveTo(-625); 
+  stepper1.setMaxSpeed(motorMax);
+  stepper1.setAcceleration(motorAccel);
+  stepper1.setSpeed(motorSpeed);
+  while (homeSeq1 == false) {
+    stepper1.moveTo(-645);
     stepper1.run();
     Serial.println(stepper1.currentPosition());
-    
-    if(stepper1.currentPosition() == -625){
+
+    if (stepper1.currentPosition() == -645) {
       homeSeq1 = true;
     }
   }
-  
+
   stepper1.setCurrentPosition(0);
   Serial.println("Set 0 Pos");
-  stepper1.setMaxSpeed(1000.0);
-  stepper1.setAcceleration(100.0);
-  stepper1.setSpeed(400);
+  stepper1.setMaxSpeed(motorMax);
+  stepper1.setAcceleration(motorAccel);
+  stepper1.setSpeed(motorSpeed);
 }
- 
+
 void loop()
 {
   //targetPosition = Serial.read();
   //Change direction at the limits
   stepper1.run();
-  if (stepper1.currentPosition() == targetPositionPSI){ 
-     preasureValue = analogRead(preasureInput); //reads the tranducer
-       preasureValue = ((preasureValue - preasureZero)*preasureTransducerMaxPSI)/(preasureMax - preasureZero); //Converts from anolog to
-       targetPositionPSI = preasureValue;
-       Serial.println(preasureValue);
-       //delay(sensorReadDelay);
+  if (stepper1.currentPosition() == targetPositionPSI) {
+    preasureValue = analogRead(preasureInput); //reads the tranducer
+    preasureValue = ((preasureValue - preasureZero) * preasureTransducerMaxPSI) / (preasureMax - preasureZero); //Converts from anolog to
+    targetPositionPSI = preasureValue;
+    Serial.println(preasureValue);
+    //delay(sensorReadDelay);
     // Will check if the TargetPosition is greater or equal to then zero to make it take the appropriate number of steps
-    if(targetPositionPSI >= 0){
+    if (targetPositionPSI >= 0) {
       targetPositionPSI = abs(targetPositionPSI);
       targetPositionPSI = targetPositionPSI * upStepps;
       // takes the absoluete value and adds it to the base position for zero so it goes to the right place.
       targetPositionPSI = abs(targetPositionPSI) + gaugeZero;
     }
     // Will check if the TargetPosition is less then zero to make it take the appropriate number of steps
-    if(targetPositionPSI < 0){
+    if (targetPositionPSI < 0) {
       targetPositionPSI = abs(targetPositionPSI);
       targetPositionPSI = targetPositionPSI * downStepps;
       // takes the absoluete value and subtracts it to the base position for zero so it goes to the right place.
